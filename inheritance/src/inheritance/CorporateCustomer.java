f52d7887ea8273f1d6acbb8bd8c30411e82768a0
CASPER <CASPER@CASPER-PC>
2021-05-20 20:34:13 +0300

    JavaDemos

---
+++ inheritance/src/inheritance/CorporateCustomer.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class CorporateCustomer extends Customer {
+	//bir class ne yapabiliyorsa sadece onları yapabilmeli***
+	
+	 String companyName;
+	 String taxNumber;
+
+}
---
+++ inheritance/src/inheritance/Customer.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Customer {
+ int id;
+ String customerNumber;
+ 
+
+ //base/super
+//üzerinde çıkarma çaprma toplama bölme yapmadığımı ifadeler "metinsel" olarak oluşturulmalıdır.
+}
+
+
